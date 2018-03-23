3e58501c27487e4abe7fd80c86b56ce8a6fe0a3b
Aimopotis <morpheustudio@gmail.com>
2018-03-23 12:39:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# JS Game Number Guess 
+Guess a **number** between **min** and **max**, add **win**.
+You can set the **number of guesses** as well the **message** that **displays** and also **try again**. Enjoy!!!
+It uses a the simple **skeleton CSS framework**.
+
+
+## Online 
+**Click** the icons to view the project online in:
+
+[![Codepen icon](img/codepen50.png)](https://codepen.io/MorpheusStudio/pen/rdwqvP) 
+ -or- 
+[![jsfiddle icon](img/jsfiddle50.png)](https://jsfiddle.net/Morpheus_Studio/g66v0q91/)
+
+## Installation
+Clone the Github repository by using 
+* [GIT](https://git-scm.com/downloads)
+```sh
+$ git clone https://github.com/Aimopotis/js-game-number-guesser.git
+$ cd js-game-number-guesser
+```
+
+## GAME Functions
+* Player must guess a number between a min and max
+* Player gets a certain amount of guesses
+* Notify the player of guesses remaining
+* Notify the player of the correct answer if loose
+* Let the player choose to play again
+
+
+## Usage 
+There are fuctions for: 
+
+* Event Listener to check your quess
+* For creating the calculate results `calculateResults()`
+* To change the UI if you won or lost `gameOver()`
+* To display message `setMessage()`
+* To set random Number `getRandomNum()`
+* Event Listener to reload page and lets you play again
+
+
+## Contributing
+All contributions all welcome
---
+++ app.js
@@ -0,0 +1,91 @@
+/*
+GAME Functions
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify the player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let the player choose to play again
+*/
+
+// Game values
+let min = 1, 
+    max = 10,
+    winningNum = getRandomNum(min, max),
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.getElementById('game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+
+// Assing UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+       location.reload();
+  }
+})
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+   
+  // Validate Input
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if( guess === winningNum){
+  // Game over - won
+  gameOver(true);
+   } else {
+  // Wrong Number
+  guessesLeft--;
+  if(guessesLeft === 0){
+  // Game Over - lost
+   gameOver(false);
+    } else {
+  // Game continue - answer wrong
+  setMessage(`${guess} is not correct ${guessesLeft}, guesses left`, 'orange');
+  }
+  };
+
+  // Clear Input
+  guessInput.value = '';
+});
+
+
+function gameOver(won){
+  let color;
+  let msg
+  won ? (color = 'green', msg = `${winningNum} is correct! YOU WIN`):(color = 'red',  msg =`You have lost, the correct Number was ${winningNum}, ${guessesLeft} guesses left`);
+ 
+  // Disable Input
+  guessInput.disabled = won;
+   // Change border color
+   guessInput.style.borderColor = color;
+   // Set Message 
+   setMessage(msg, color);
+  
+   // Play Again?
+   guessBtn.value = 'Play Again';
+   guessBtn.className +='play-again';
+  }
+
+
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
+
+// Get Random Number
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
